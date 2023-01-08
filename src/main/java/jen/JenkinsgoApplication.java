da1dc949f80e1e8a4852af4cafa80f051bc3bd4a
opi6opi6 <opi6@hanmail.net>
2023-01-09 03:03:44 +0900

    first

---
+++ src/main/java/jen/JenkinsgoApplication.java
@@ -0,0 +1,13 @@
+package jen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jen/JenkinsgoApplicationTests.java
@@ -0,0 +1,13 @@
+package jen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
